3ca83c4641be1fdd395a8581d4adf899a9ec7865
Andres Pardini <unpardos@gmail.com>
2018-04-27 00:05:39 -0300

    add missing components

---
+++ src/components/Break.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="columns is-centered break-wrap">
+    <div class="column has-text-centered text break-divider">
+      <div class="break-text">BREAK</div>
+      <div class="break-time">12:00HS</div>
+    </div>
+  </div>
+</template>
